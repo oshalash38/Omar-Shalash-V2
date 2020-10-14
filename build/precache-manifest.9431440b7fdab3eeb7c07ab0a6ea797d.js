a94e910db5ae92af02445435c0a2ef93490b8fcf
oshalash38 <omarshalash@live.com>
2020-10-14 14:30:53 -0400

    adjusting firebase hosting

---
+++ build/precache-manifest.9431440b7fdab3eeb7c07ab0a6ea797d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a5dfa7f5742401830a13e41f6b2416",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6213014eea86ccec2dd",
+    "url": "/static/css/main.9b2a1cc4.chunk.css"
+  },
+  {
+    "revision": "28ee14147b34eab9d20a",
+    "url": "/static/js/2.6ddb0441.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/static/js/2.6ddb0441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6213014eea86ccec2dd",
+    "url": "/static/js/main.10e62882.chunk.js"
+  },
+  {
+    "revision": "60f79e47ebad919c2616",
+    "url": "/static/js/runtime-main.22d1b636.js"
+  },
+  {
+    "revision": "e8cfe56ccaa6ebf71d57fd3173ee4183",
+    "url": "/static/media/Alvi_Nastaleeq_Regular.e8cfe56c.ttf"
+  },
+  {
+    "revision": "7430d922b9ae0f89f6465833d9bc2f1a",
+    "url": "/static/media/Omar_Shalash_Resume.7430d922.pdf"
+  },
+  {
+    "revision": "3bf1e41fcc5d5357cd15890505bbfcb8",
+    "url": "/static/media/calendar.3bf1e41f.jpg"
+  },
+  {
+    "revision": "b62a1bca96231d34640afe3601e5183a",
+    "url": "/static/media/code.b62a1bca.jpg"
+  },
+  {
+    "revision": "b0f28c9f4bc120f9a96ccef6f19e261d",
+    "url": "/static/media/ice.b0f28c9f.png"
+  },
+  {
+    "revision": "67f805aa1ca55bc3be2781c15d9877fe",
+    "url": "/static/media/ideas.67f805aa.jpg"
+  },
+  {
+    "revision": "5e2212478fa2c3c639722b89e126396a",
+    "url": "/static/media/meet.5e221247.jpg"
+  },
+  {
+    "revision": "60b1195d2c651b499d188c8a0ab12d19",
+    "url": "/static/media/xo.60b1195d.jpg"
+  }
+]);
